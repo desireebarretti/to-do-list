31b03b9ae4e3dd7008c6da3893b705066095a6d4
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-12 20:58:17 -0300

    Criando o dlbClique

--- function.js
+++ function.js
@@ -1,12 +1,21 @@
 let addItem = document.querySelector('#input__item')
 let botaoItem = document.querySelector('#btn__add')
 let itensAdicionados =  document.querySelector('#adicionado')
+let img = document.querySelector('#img')
 
 function adicionarItem() {
    
     if (addItem.value === ''){
         alert('Você precisa adicionar um item!')
+
     } else {
         itensAdicionados.innerHTML = addItem.value  
+        itensAdicionados.style.display = 'block'
     }
+    addItem.value = ''
+}
+
+function duploCliqueChecado(){
+
+    itensAdicionados.style.display = 'none'
 }
