bb68063a070b395f9ef61c5dcd13b134bd295acc
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-14 15:55:47 -0300

    Tentando acessar o imprimir na tela o local storange

--- function.js
+++ function.js
@@ -1,19 +1,25 @@
 let addItem = document.querySelector('#input__item')
 let botaoItem = document.querySelector('#btn__add')
-let itensAdicionados =  document.querySelector('#adicionado')
+let itensAdicionados = document.querySelector('#adicionado')
 let img = document.querySelector('#img')
 let botaoCheck = document.querySelector('#btn__check')
 
+const addLocalStorange = []
 
 function adicionarItem() {
-   
-    if (addItem.value === ''){
+
+    if (addItem.value === '') {
         alert('Você precisa adicionar um item!')
         addItem.style.border = '1px solid #fa3737b7'
         addItem.style.focus
 
     } else {
-        
+        addLocalStorange.push(addItem.value)
+
+        console.log(addLocalStorange)
+
+        localStorage.setItem(addLocalStorange, itensAdicionados)
+
         // itensAdicionados.innerHTML = addItem.value  
         // itensAdicionados.style.display = 'block'
         // botaoCheck.style.display = 'block'
@@ -21,7 +27,7 @@ function adicionarItem() {
     addItem.value = ''
 }
 
-function duploCliqueChecado(){
+function duploCliqueChecado() {
     itensAdicionados.style.display = 'none'
 }
 
