84d4b52f569339abc594a89c24a8be99385c06e6
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-17 20:16:56 -0300

    Tentando o delete de um item da lista

--- Segunda vida projeto/function.js
+++ Segunda vida projeto/function.js
@@ -1,8 +1,10 @@
 let botaoAdicionarItem = document.querySelector('#btn__add')
 let inputItem = document.querySelector('#input__item')
+let criarLi = document.querySelector('#ul__addItem')
 const localStorageKey = 'chave-local-storange'
 
 function btn__addItem() {
+
     if (inputItem.value == '') {
         alert('Digite o item para adicionar na sua lista!')
 
@@ -11,12 +13,11 @@ function btn__addItem() {
         inputItem.style.border = 'none'
         mostrarValoresTela()
     }
-    
+
 }
 
 function mostrarValoresTela() {
     let valores = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
-    let criarLi = document.querySelector('#ul__addItem')
 
     valores.push(
         {
@@ -29,7 +30,14 @@ function mostrarValoresTela() {
         criarLi.style.display = 'block'
 
         // colocando a let valores, na posição atual, e na propriedade valor
-        criarLi.innerHTML = `<li>${valores[i]['valor']}</li>`
+        criarLi.innerHTML +=
+            `<li id="li__addItem" ondblclick="removerItem()">
+                ${valores[i]['valor']}
+            </li>`
     }
 
+}
+
+function removerItem(data) {
+    
 }
