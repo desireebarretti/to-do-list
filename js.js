556750f9ccf4fe9d5342c433777a519d0c68a0d0
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-24 21:25:57 -0300

    Adicionando favicon, uma funcionalidade e estilizando a section itens

--- js.js
+++ js.js
@@ -1,56 +1,6 @@
-// nome da chave
-const localStorageKey = 'to-do-list'
-
-let list = document.querySelector('#adicionado')
+let input = document.querySelector('#input')
+let itensAdd = document.querySelector('#itens')
 
 function novaTarefa() {
-    let input = document.querySelector('#input__item')
-
-    if(!input.value){
-        alert('Você precisa digitar algo!')
-        input.style.border = '1px solid #fa3737b7'
-        
-    } else {
-        input.style.border = 'none'
-        let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
-
-        values.push(
-            {
-                valor: input.value
-            }
-        )
-        localStorage.setItem(localStorageKey, JSON.stringify(values))
-
-        mostrarValores()
-    }
-    input.value = ''
+    itensAdd.style.display = 'flex'
 }
-
-function mostrarValores() {
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
-
-    list.innerHTML = ''
-
-    for(let i = 0; i < values.length; i++) {
-
-       list.innerHTML += `<li id="lista__li" ondblclick="remover('${values[i]['valor']}')"> ${values[i]['valor']} </li>`
-
-       list.style.display = 'block'
-
-    }
-}
-
-function remover(data) {
-
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
-
-    let index = values.findIndex(x => x.name == data)
-    console.log(index)
-    values.splice(index,1)
-    localStorage.setItem(localStorageKey, JSON.stringify(values))
-
-    mostrarValores()
-}
-
-
-mostrarValores()
