9dcb422dced1c095a0a4f38d2e73bf75ed59e56c
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-25 17:58:33 -0300

    Exibindo na tela o valor do input

--- js.js
+++ js.js
@@ -1,6 +1,18 @@
 let input = document.querySelector('#input')
 let itensAdd = document.querySelector('#itens')
+let todasPartesItem = document.querySelector('#partes-item')
+let paragrafo = document.querySelector('#paragrafo')
 
 function novaTarefa() {
     itensAdd.style.display = 'flex'
+    todasPartesItem.style.display = 'flex'
+    paragrafo.innerHTML = input.value
+}
+
+function tarefaChecada() {
+    paragrafo.style.
+}
+
+function limparTarefa() {
+    itensAdd.style.display = 'none'
 }
