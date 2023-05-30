df3802c09957169fa9c4ed187ae4578f11b73c1b
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-30 15:43:56 -0300

    Montando o array

--- js.js
+++ js.js
@@ -1,16 +1,29 @@
 let input = document.querySelector('#input')
 let addItens = document.querySelector('#itens')
 
+let itens = []
 
-function novaTarefa() {
-    if(input.value == ''){
-        alert('Digite algo')
+function novaTarefa(evento) {
+    if (input.value == '') {
+        alert('Digite um item')
         input.style.border = '1px solid red'
     } else {
+        input.style.border = 'none'
         addItens.style.display = 'flex'
         addItens.innerHTML = input.value
+
+        itens.push(input.value)
+        console.log(itens)
+
+        // for (let i = 0; i < itens.length; i++) {
+        //     let criarLi = document.createElement('li')
+        //     addItens.appendChild =
+        //     criarLi = valorInput
+        //     console.log(criarLi)
+        // }
+
     }
-    
+    input.value = ''
 }
 
 function limparTarefa() {
